804aacfc883332b44c1aa45e1e4dbda7167af363
xaiti <xaiti75@gmail.com>
2021-09-03 13:17:48 +0100

    added password reset token and date to user schema

--- models/user.js
+++ models/user.js
@@ -5,7 +5,9 @@ var stringRequired = { type: String, required: true }
 const userSchema = new mongoose.Schema({
     name: stringRequired,
     email: stringRequired,
-    password: stringRequired
+    password: stringRequired,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
 })
 
 module.exports = mongoose.model('User', userSchema)
